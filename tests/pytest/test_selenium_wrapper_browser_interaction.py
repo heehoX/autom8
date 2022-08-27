bc159314d1ade36a2511c300a8c43e1d6d0beaa6
Arvin <arvinjohnyadao@hotmail.com>
2022-08-27 15:19:46 +0800

    Removed unused imports

--- tests/pytest/test_selenium_wrapper_browser_interaction.py
+++ tests/pytest/test_selenium_wrapper_browser_interaction.py
@@ -1,8 +1,4 @@
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.firefox import GeckoDriverManager
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 from autom8.core.ui.selenium.selenium_driver_factory import SeleniumDriverFactory
 from autom8.core.ui.selenium.selenium_wrapper import SeleniumWrapper
