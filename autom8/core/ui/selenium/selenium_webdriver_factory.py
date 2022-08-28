55e25bbbbaf59d77a1089bfc4390b2c233b45117
Arvin <arvinjohnyadao@hotmail.com>
2022-08-28 23:58:33 +0800

    Decide headless mode by reading env variable

--- autom8/core/ui/selenium/selenium_webdriver_factory.py
+++ autom8/core/ui/selenium/selenium_webdriver_factory.py
@@ -1,3 +1,5 @@
+import os
+
 from selenium import webdriver
 
 from webdriver_manager.chrome import ChromeDriverManager
@@ -18,18 +20,22 @@ from autom8.core.enums.browser import Browser
 class SeleniumDriverFactory:
     @staticmethod
     def create_new_driver(browser: str):
+        # TODO: make this a part of config.
+        # Temporary way to force github actions to always run headless.
+        is_headless = os.getenv("headless", 'False').lower() in ('true', '1', 't')
+
         match browser:
             case Browser.CHROME.value:
                 options = ChromeOptions()
-                options.headless = True
+                options.headless = is_headless
                 return webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
             case Browser.FIREFOX.value:
                 options = FirefoxOptions()
-                options.headless = True
+                options.headless = is_headless
                 return webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=options)
             case Browser.EDGE.value:
                 options = EdgeOptions()
-                options.headless = True
+                options.headless = is_headless
                 return webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()), options=options)
             case _:
                 raise Exception(f"Unsupported selenium web driver type: {browser}")
